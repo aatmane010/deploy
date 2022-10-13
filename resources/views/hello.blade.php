c68fdba487db63efb2661d27df865411c3dd20a7
aatmane010 <aatmaneoulmouden21@gmail.com>
2022-10-13 22:45:41 +0100

    initial commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello page</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <img src="{{ asset('img/2022-bmw-x6.jpg') }}" alt="">
+    <h1 class="primary">This is the HELLO page</h1>
+</body>
+
+</html>
